7462ad51db0888088a37516474bd7fb0141771b1
Dave <ddawid@windowslive.com>
2019-11-20 23:13:08 +0100

    Add custom pipe to remove - from product code

--- ProductListApplication/src/app/app.module.ts
+++ ProductListApplication/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsListComponent } from './products/products-list/products-list.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsListComponent
+    ProductsListComponent,
+    ConvertToSpacesPipe
   ],
   imports: [
     BrowserModule,
---
+++ ProductListApplication/src/app/shared/convert-to-spaces.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ConvertToSpacesPipe } from './convert-to-spaces.pipe';
+
+describe('CovertToSpacesPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ConvertToSpacesPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
